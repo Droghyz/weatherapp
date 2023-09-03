9ffab560fe50fd6f8aba8d57abcb7aed518042e2
Droghyz <m.di.fatta08@gmail.com>
2023-09-03 18:16:50 +0200

    Added JS for icons and weathercode

--- src/js/controller.js
+++
@@ -1,7 +0,0 @@
-// import "core-js/stable";
-// import "regenerator-runtime/runtime.js";
-// import * as model from "./model.js";
-// import { View } from "./view.js";
-// import { async } from "regenerator-runtime";
-
-// model.getPosition();
